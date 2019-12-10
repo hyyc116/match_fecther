9128e2a68bf7204cc2d3ae623c34ab8934c378df
hyyc116 <hyyc116@gmail.com>
2019-12-10 11:16:11 +0800

    add

--- match_fecther.py
+++ match_fecther.py
@@ -118,7 +118,9 @@ def iter_fetch_seq(seq_num=None):
             count+=len(matches)
             matches=[]
 
-        time.sleep(5)
+            time.sleep(5)
+
+        time.sleep(1)
 
     if len(matches)>0:
         open('matches.txt','a').write('\n'.join(matches)+'\n')
